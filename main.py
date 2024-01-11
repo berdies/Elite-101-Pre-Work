16ba32cfbe2259775a5aa395925023d485a48858
berdies <132856091+berdies@users.noreply.github.com>
2024-01-11 02:32:59 +0000

    Completing basics of chatbot
    Chatbot template completed,  user's name and age collected, display menu completed.

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 # Elite 101 Pre-Work Project
+
+#Introduction to chatbot and receiving the user's name and age
+#Initializing the conversation
+print('Welcome to THE chatbot!')
+name = input("For starters, What is your name? ")
+age = int(input("How old are you? "))
+
+print(f'\nHello {name}! Nice to meet you.\n{age}, my lucky number!')
+print('\nHow can I help you?')
+
+#Displaying conversation options for the user
+def display_menu():
+  print('\n1. Whats your favorite color?')
+  print('2. What is your favorite food?')
+  print('3. What is your favorite movie?')
+  print('4. Exit')
+
+
+
+
+
+
+  
